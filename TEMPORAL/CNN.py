30a9fd5b6b6546555819301f731e1096cf40aa48
Amith Korada <akorada@iu.edu>
2024-12-16 17:11:27 -0500

    Modified CNN

--- TEMPORAL/CNN.py
+++ TEMPORAL/CNN.py
@@ -16,16 +16,37 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # Set GPU to use
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'  # Allow GPU memory to grow as needed
 
 def get_files(folder_path):
+    """
+    Load the data from the given folder path and return the training and testing data.
 
+    Parameters
+    ----------
+    folder_path : str
+        The path to the folder containing the data.
+
+    Returns
+    -------
+    X_train : ndarray
+        The training data.
+    X_test : ndarray
+        The testing data.
+    y_train : ndarray
+        The labels for the training data.
+    y_test : ndarray
+        The labels for the testing data.
+    """
+    # Load the data
     X_train_coke = np.load(os.path.join(folder_path, 'COKE_TRAIN.npz'))
     X_train_sal = np.load(os.path.join(folder_path, 'SAL_TRAIN.npz'))
 
+    # Create the labels
     y_train_coke = np.ones(X_train_coke['neuron_data'].shape[0])
     y_train_sal = np.zeros(X_train_sal['neuron_data'].shape[0])
 
     X_test_coke = np.load(os.path.join(folder_path, 'COKE_TEST.npz'))
     X_test_sal = np.load(os.path.join(folder_path, 'SAL_TEST.npz'))
 
+    # Create the labels
     y_test_coke = np.ones(X_test_coke['neuron_data'].shape[0])
     y_test_sal = np.zeros(X_test_sal['neuron_data'].shape[0])
 
@@ -35,6 +56,7 @@ def get_files(folder_path):
     print('X_test_coke shape :', X_test_coke['neuron_data'].shape)
     print('X_test_sal shape :', X_test_sal['neuron_data'].shape)
 
+    # Stack the data
     X_train = np.vstack([X_train_coke['neuron_data'], X_train_sal['neuron_data']])
     X_test = np.vstack([X_test_coke['neuron_data'], X_test_sal['neuron_data']])
 
@@ -49,12 +71,34 @@ def get_files(folder_path):
 
     return X_train, X_test, y_train, y_test
 
-def predict(model_path, X_test, y_test):
 
+def predict(model_path, X_test, y_test):
+    """
+    Load a trained model and make predictions on the test data.
+
+    Parameters
+    ----------
+    model_path : str
+        The path to the trained model file.
+    X_test : ndarray
+        The test data input features.
+    y_test : ndarray
+        The true labels for the test data.
+
+    Returns
+    -------
+    None
+    """
+    # Load the model
     model = keras.models.load_model(model_path)
 
-    predictions  = model.predict(X_test)
+    # Make predictions on the test data
+    predictions = model.predict(X_test)
+
+    # Convert probabilities to binary predictions
     predictions = (predictions > 0.5).astype(int)
+
+    # Print the classification report
     print(classification_report(y_test, predictions))
 
 def plt_hisory(hist, figname):
@@ -117,21 +161,39 @@ def plt_hisory(hist, figname):
     #plt.show()
 
 def cnn_model(X_train, X_test, y_train, y_test, folder_path):
+    """
+    Builds, trains, and evaluates a CNN model.
+
+    Parameters:
+    X_train (ndarray): Training data input features.
+    X_test (ndarray): Testing data input features.
+    y_train (ndarray): Training data labels.
+    y_test (ndarray): Testing data labels.
+    folder_path (str): Path to save model files.
+
+    Returns:
+    tuple: A tuple containing the training history, best model path, and last model path.
+    """
     # Model Implementation
     model = Sequential()
+    # First convolutional layer with 64 filters, kernel size of 3, and ReLU activation
     model.add(Conv1D(64, kernel_size=3, activation='relu', input_shape=X_train.shape[1:]))
+    # Max pooling layer
     model.add(MaxPooling1D(pool_size=2))
+    # Flatten the output of the convolutional and pooling layers
     model.add(Flatten())
+    # Dense layer with 32 neurons and ReLU activation
     model.add(Dense(32, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))  # Use 'sigmoid' for binary classification
+    # Output layer with sigmoid activation for binary classification
+    model.add(Dense(1, activation='sigmoid'))
 
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     print(model.summary())
-    best_model_path = os.path.join(folder_path,'CNN1_best_model.keras')
+
     # Callback to save the best model
-    model_checkpoint = ModelCheckpoint(filepath=best_model_path, monitor='val_loss', 
-                                    save_best_only=True, verbose=1)
+    best_model_path = os.path.join(folder_path,'CNN1_best_model.keras')
+    model_checkpoint = ModelCheckpoint(filepath=best_model_path, monitor='val_loss', save_best_only=True, verbose=1)
 
     # Train the model
     hist = model.fit(X_train, y_train, epochs=50, batch_size=2, validation_data=(X_test, y_test), callbacks = [model_checkpoint])
@@ -140,13 +202,29 @@ def cnn_model(X_train, X_test, y_train, y_test, folder_path):
     loss, accuracy = model.evaluate(X_test, y_test)
     print(f'Test Accuracy: {accuracy:.4f}')
 
+    # Save the last model
     last_model_path = os.path.join(folder_path,'CNN1_last_model.keras')
     model.save(last_model_path)
+
+    # Make predictions with the last model
     predict(last_model_path, X_test, y_test)
 
     return hist, best_model_path, last_model_path
 
 def cnn_model_2(X_train, X_test, y_train, y_test, folder_path):
+    """
+    Builds, trains, and evaluates a CNN model with regularization.
+
+    Parameters:
+    X_train (ndarray): Training data input features.
+    X_test (ndarray): Testing data input features.
+    y_train (ndarray): Training data labels.
+    y_test (ndarray): Testing data labels.
+    folder_path (str): Path to save model files.
+
+    Returns:
+    tuple: A tuple containing the training history, best model path, and last model path.
+    """
     # Model Implementation with Regularization
     model = Sequential()
     model.add(Conv1D(64, kernel_size=3, activation='relu', input_shape=X_train.shape[1:]))
@@ -160,21 +238,24 @@ def cnn_model_2(X_train, X_test, y_train, y_test, folder_path):
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     print(model.summary())
-    best_model_path = os.path.join(folder_path, 'CNN2_best_model.keras')
     
-    # Callbacks
-    model_checkpoint = ModelCheckpoint(filepath=best_model_path, monitor='val_loss', 
+    # Prepare file paths for saving models
+    best_model_path = os.path.join(folder_path, 'CNN2_best_model.keras')
+    last_model_path = os.path.join(folder_path, 'CNN2_last_model.keras')
+    
+    # Callbacks for model training
+    model_checkpoint = ModelCheckpoint(filepath=best_model_path, monitor='val_loss',
                                        save_best_only=True, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
     lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1)
     
     # Train the model
     hist = model.fit(
-        X_train, y_train, 
-        epochs=50, 
+        X_train, y_train,
+        epochs=50,
         batch_size=16,  # Increased batch size
-        validation_data=(X_test, y_test), 
-        callbacks=[model_checkpoint]
+        validation_data=(X_test, y_test),
+        callbacks=[model_checkpoint, early_stopping, lr_scheduler]
     )
     
     # Evaluate the model
@@ -184,12 +265,27 @@ def cnn_model_2(X_train, X_test, y_train, y_test, folder_path):
     # Detailed classification report
     predict(best_model_path, X_test, y_test)
     
-    last_model_path = os.path.join(folder_path, 'CNN2_last_model.keras')
+    # Save the last model
     model.save(last_model_path)
     
     return hist, best_model_path, last_model_path
 
 def rnn_model(X_train, X_test, y_train, y_test, folder_path):
+    """
+    Defines a RNN model architecture with three bidirectional LSTM layers and a dense output layer.
+    
+    Parameters:
+    X_train (ndarray): Training data input features.
+    X_test (ndarray): Testing data input features.
+    y_train (ndarray): Training data labels.
+    y_test (ndarray): Testing data labels.
+    folder_path (str): Path to the folder where model output will be saved.
+    
+    Returns:
+    hist (dict): History of the model training.
+    best_model_path (str): Path to the best model saved during training.
+    last_model_path (str): Path to the last model saved during training.
+    """
     
     n_outputs = 1  # Assuming binary classification
     # Define the time-series input layer
